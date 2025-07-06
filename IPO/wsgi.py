b4d56648816540e413c98d925311bb6a6c3a300b
Mansi Sharma <mansisharma@Mansis-MacBook-Air.local>
2025-07-06 23:54:39 +0530

    Changes

--- IPO/wsgi.py
+++ IPO/wsgi.py
@@ -6,11 +6,18 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# 👇 Auto-migrate workaround
+import django
+from django.core.management import call_command
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IPO.settings')
+django.setup()
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration error:", e)
 
 application = get_wsgi_application()
